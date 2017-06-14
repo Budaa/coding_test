252a3a7768354397b90017d4d942187d8d36b642
Paweł Buderaski <pbuderaski@gmail.com>
2017-06-14 22:58:09 +0100

    Added Footer

---
+++ app/components/AppFooter/Footer.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Footer = styled.footer`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  padding: 1rem;
+  color: #ababbe;
+  background-color: white;
+  border-top: 1px solid #dedede;
+  font-size: 0.9em;
+`;
+export default Footer;
---
+++ app/components/AppFooter/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Footer from './Footer';
+
+class AppFooter extends React.Component {
+  render() {
+    return (
+      <Footer>
+        <div>
+          Your data is secured by TrueLayer
+        </div>
+        <div>
+          Tearms of user | Privacy Policy
+        </div>
+      </Footer>
+    );
+  }
+}
+
+export default AppFooter;
