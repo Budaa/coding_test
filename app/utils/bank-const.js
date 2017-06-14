245f32202a71a50fa85096d11965c888a494ab07
Paweł Buderaski <pbuderaski@gmail.com>
2017-06-14 22:43:43 +0100

    created file exporting bank images

---
+++ app/utils/bank-const.js
@@ -0,0 +1,10 @@
+import hsbc from '../assets/images/hsbc.svg';
+import barclays from '../assets/images/barclays.svg';
+import coOp from '../assets/images/co-op.svg';
+import halifax from '../assets/images/halifax.svg';
+import nationWide from '../assets/images/nationwide.svg';
+import natWest from '../assets/images/natwest.svg';
+import rbs from '../assets/images/rbs.svg';
+import santander from '../assets/images/santander.svg';
+
+export default { hsbc, barclays, coOp, halifax, nationWide, natWest, rbs, santander };
