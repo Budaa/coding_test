cee208604fc9dddb72005d772e0e30084fc2316c
Paweł Buderaski <pbuderaski@gmail.com>
2017-06-14 22:59:30 +0100

    Updated HomePage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -1,24 +1,40 @@
-/*
- * HomePage
- *
- * This is the first thing users see of our App, at the '/' route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
- */
-
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import { connect } from 'react-redux';
+import AppNav from 'components/AppNav';
+import AppFooter from 'components/AppFooter';
+import InfoContainer from 'components/InfoContainer';
+import ProvidersList from 'components/ProvidersList';
+import { getProvidersAttempt } from './actions';
+import tamplateLogo from '../../assets/images/template-logo.png';
+import Banks from '../../utils/banks-const';
 
-export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+class HomePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const { providers } = this.props;
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <div>
+        <AppNav
+          backButton={false}
+          title={'Choose your Bank'}
+
+        />
+        <InfoContainer logo={tamplateLogo} />
+        <ProvidersList {...{ providers }} />
+        <div onClick={() => this.props.getProvidersAttempt()}>dasdasdasdad</div>
+        <AppFooter />
+      </div>
     );
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  getProvidersAttempt: () => dispatch(getProvidersAttempt()),
+});
+
+const mapStateToProps = (state) => ({
+  providers: state.home && state.home.providers,
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+
