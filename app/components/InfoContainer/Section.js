62a65405baf0d81acef076a30fd71d3c2e4a2ee4
Paweł Buderaski <pbuderaski@gmail.com>
2017-06-15 01:16:27 +0100

    Typo fixed

--- app/components/InfoContainer/Section.js
+++ app/components/InfoContainer/Section.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 const Section = styled.section`
   width: 100%;
   display: flex;
-  flex-diraction: column;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: #f5f7fa;
+  padding: 50px;
 `;
 
 export default Section;
