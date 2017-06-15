33da061fb2527a4684bb23d8631b5a132c84518e
Paweł Buderaski <pbuderaski@gmail.com>
2017-06-15 01:55:24 +0100

    Component and styling for providers list

---
+++ app/components/ProvidersList/Container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  align-itmes: center;
+`;
+
+export default Container;
--- app/components/ProvidersList/index.js
+++ app/components/ProvidersList/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import BankButton from 'components/BankButton';
+import Banks from '../../utils/banks-const';
+import Container from './Container';
 
-const InfoContainer = () => (
-  <div>
-    Providers
-  </div>
+const ProvidersList = ( { providers } ) => (
+  <Container>
+    {providers && providers.size &&
+      providers.map((provider, key) => <BankButton key={key} alt={provider.get('displayName')} src={Banks[provider.get('connectorId')]} />)
+    }
+  </Container>
 );
 
-export default InfoContainer;
+export default ProvidersList;
